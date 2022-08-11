3e6dc5d848ffbcab59f0b2cee92a3c5903e733ee
anwarkvzk <anvarvzk521@gmail.com>
2022-08-11 20:04:08 +0530

    Express and mongodb connnection confiqured

--- server/index.js
+++ server/index.js
@@ -2,10 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRouter from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts', postRouter);
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const CONNECTION_URL =
+  "mongodb+srv://anwar:anwar123456@cluster0.u5sgows.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
 
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+
+  mongoose.connect(CONNECTION_URL).then(()=>{console.log('...')})
